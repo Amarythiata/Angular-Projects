ecae37adaf924a32c3b92617ca55944c3eedd425
guisse <amarythiata@gmail.com>
2023-01-24 16:34:47 +0100

    awesome app

---
+++ angular-hightchart/HightChartApp/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HighchartsChartComponent } from 'highcharts-angular';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HighchartsChartComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
